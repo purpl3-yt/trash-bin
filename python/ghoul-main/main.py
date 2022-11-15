f853ffcd61d19bda40fa3d1594586b60da3c273f
purpl3-yt <remezdevid321@gmail.com>
2022-11-15 17:28:14 +0200

    ghoul update

--- python/ghoul-main/main.py
+++ python/ghoul-main/main.py
@@ -1,4 +1,5 @@
 from time import sleep as wait
+import os
 
 print('''
 You are ghoul?
@@ -10,7 +11,7 @@ ghoul = input('Select: ')
 sum = 1000
 minus = 7
 
-if str(ghoul).lower() in ['yes','y','1']:
+if str(ghoul).lower() in ['yes','y','1','да','н']:
     wait(0.5)
     print("I'm ghoul")
     wait(0.4)
@@ -21,7 +22,10 @@ if str(ghoul).lower() in ['yes','y','1']:
             wait(0.025)
         else:
             print(f'6 - 7 = 0')
+            wait(0.025)
+            print('0')
+            os.system('pause')
             break
 
-elif str(ghoul).lower() in ['no','n','2']:
+elif str(ghoul).lower() in ['no','n','2','нет','т']:
     print("You aren't ghoul :(")
