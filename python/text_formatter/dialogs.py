7ba93d99cbf2bc90d62cb022732be1606d446f16
purpl3-yt <remezdevid321@gmail.com>
2022-11-22 23:23:36 +0200

    123456789

--- python/text_formatter/dialogs.py
+++ python/text_formatter/dialogs.py
@@ -1,23 +1,11 @@
 import funcs
 
-modes_dict = {}
-
+modes_list = []
 
 for fun in dir(funcs):
     if not '_' in fun:
-        try:
-            exec(f'var = funcs.{fun}("{fun}");modes_dict["{fun}"] = [var,funcs.{fun}]')
-        except TypeError:
-            exec(f'modes_dict["{fun}"] = ["{fun}",funcs.{fun}]')
+        modes_list.append(fun)
 
-def get_dialog():
-    index = 1
-    messages = []
-    for key,mode in list(modes_dict.items()):
-        try:
-            messages.append(str(index)+') '+mode[1](key))
-        except TypeError:
-            messages.append(str(index)+') '+str(key).capitalize())
-        index+=1
-    return messages
-print('\n'.join(get_dialog()))
+def generate_help():
+    help = ', '.join(modes_list)
+    return help
--- python/text_formatter/main.py
+++ python/text_formatter/main.py
@@ -1,8 +1,25 @@
-import dialogs
+import os,platform
+from dialogs import *
+from colorama import Fore
+
+def cls():
+    if platform.system().lower() == 'windows':
+        os.system('cls')
+    elif platform.system().lower() == 'linux':
+        os.system('clear')
+
+def gprint(text):
+    print(Fore.GREEN+text+Fore.RESET)
+
+gprint('Hello! its text formater')
 
 while True:
-    text = input('Hello! its text formater\nEnter your text: ')
-    mode = input('Enter mode (for help enter "help"): ')
+    text = input(Fore.GREEN+'Enter your text: '+Fore.RESET)
+    mode = input(Fore.GREEN+'Enter mode (for help enter "help"): '+Fore.RESET)
+    if str(mode).lower() in ['h','help','р']:
+        print(Fore.CYAN+generate_help()+Fore.RESET)
 
-    #if mode=='help':
-        
+    else:
+        func = getattr(funcs, modes_list.index(mode))
+        result = func(text)
+        print(Fore.CYAN+result+Fore.RESET)
