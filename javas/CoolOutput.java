0220a922f53d928a04d627bbd24d1e32459790fc
purpl3-yt <remezdevid321@gmail.com>
2022-11-22 20:37:34 +0200

    add java program

---
+++ javas/CoolOutput.java
@@ -0,0 +1,24 @@
+package javas;
+
+import java.util.Scanner;
+
+public class CoolOutput {
+    
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String temp = sc.next();
+        String str = temp+(temp.substring(temp.length()-1,temp.length()));
+        int what = str.length()-1;
+        for (int i = 0; i < what;) {
+            StringBuilder strBuilder = new StringBuilder(str);
+            strBuilder.deleteCharAt(what - i);
+           str = strBuilder.toString();
+            System.out.println(strBuilder.toString());
+           i++;
+           Thread.sleep(25);
+        }
+        sc.close();
+    }
+
+}
