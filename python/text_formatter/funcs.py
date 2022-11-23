96527b42b79fe14385564e99fb8767e353ac2236
purpl3-yt <remezdevid321@gmail.com>
2022-11-23 12:15:38 +0200

    new ver?

--- python/text_formatter/funcs.py
+++ python/text_formatter/funcs.py
@@ -7,5 +7,20 @@ def lowercase(text):
 def capitalize(text):
     return str(text).capitalize()
 
+def ladder(text):
+    slicer = len(text)
+    steps = []
+    for i in range(slicer):
+        steps.append(str(text)[:slicer])
+        slicer-=1
+    return steps
+
+def ladder2(text):
+    steps = ladder(text)
+    steps2 = ladder(text)[::-1]
+    steps.pop(len(steps)-1)
+    return steps+steps2
+
+
 def replace(text,what,new):
     return str(text).replace(what,new)
--- python/text_formatter/main.py
+++ python/text_formatter/main.py
@@ -11,15 +11,38 @@ def cls():
 def gprint(text):
     print(Fore.GREEN+text+Fore.RESET)
 
+def cprint(text):
+    print(Fore.CYAN+text+Fore.RESET)
+
+def rprint(text):
+    print(Fore.RED+text+Fore.RESET)
+
+def getmode():
+    return input(Fore.GREEN+'Enter mode (for help enter "help"): '+Fore.RESET)
+
+def gettext():
+    return input(Fore.GREEN+'Enter your text: '+Fore.RESET)
+
 gprint('Hello! its text formater')
 
-while True:
-    text = input(Fore.GREEN+'Enter your text: '+Fore.RESET)
-    mode = input(Fore.GREEN+'Enter mode (for help enter "help"): '+Fore.RESET)
-    if str(mode).lower() in ['h','help','р']:
-        print(Fore.CYAN+generate_help()+Fore.RESET)
+err_mode = False
 
+while True:
+    if not err_mode:
+        text = gettext()
+    mode = getmode()
+    if str(mode).lower() in ['h','help','р']:
+        cprint(generate_help())
     else:
-        func = getattr(funcs, modes_list.index(mode))
-        result = func(text)
-        print(Fore.CYAN+result+Fore.RESET)
+        try:
+            func = getattr(funcs, modes_list[modes_list.index(mode)])
+        except ValueError:
+            rprint('No such mode: '+mode)
+            err_mode=True
+        else:
+            err_mode=False
+            result = func(text)
+            if type(result) == list:
+                cprint('\n'.join(result))
+            elif type(result) == str:
+                cprint(result)
