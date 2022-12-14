3c72ea41b9b9541b29751123ec271e24944609b0
purpl3-yt <remezdevid321@gmail.com>
2022-12-14 22:35:51 +0200

    add prog

--- python/footprints-detector/main.py
+++ python/footprints-detector/main.py
@@ -1,13 +1,14 @@
 from urllib.parse import urlparse
 import googlesearch
 import os,sys
+import base64
 
 os.chdir(sys.path[0])
 
 log_file = 'log.txt'
 
-def generate_google(text: str):#unused
-    return 'https://www.google.com/search?q='+text
+def print_result(result):
+    print(f'Found an {urlparse(result).hostname}, url: '+result)
 
 def check_log(query: str):
     with open(log_file,'r') as log:
@@ -23,8 +24,23 @@ def save_result(site: str,url: str):
         with open(log_file,'a') as log:
             log.write(str_to_save+'\n')
 
+nsfw = False
+
+nsfw_strings = ['cG9ybg==','c2V4','cG9ybm8=','eHZpZGVv']
+decoded_nsfw_strings = []
+for nsfw_string in nsfw_strings:
+    decoded_nsfw_strings.append(bytes(base64.b64decode(nsfw_string)).decode('utf-8'))
+
+def check_nsfw(result,nsfw,nsfw_count):
+    if nsfw:
+        for string in decoded_nsfw_strings:
+            if string in result:
+                print('Nsfw url found!')
+                nsfw_count+=1
+                return True
+
 while True:
-    nickname = input('Enter an nickname (2 clear log): ')
+    nickname = input('Enter an nickname ("2" clear log, "3" enable nsfw results): ')
     #nickname = 'PLNT'
     #results = input('Enter an count of results (int): ')
     results = 100
@@ -36,14 +52,35 @@ while True:
                 log.write('')
         except FileNotFoundError:
             pass
+    
+    elif nickname=='3':
+        if nsfw:
+            nsfw=False
+            print('Disable nsfw')
+        elif not nsfw:
+            nsfw=True
+            print('Enable nsfw')
+
+
     else:
         if nickname!='':
             break
         else:
             print('Enter Nickname!')
 
-for result in search:
+count = 0
+nsfw_count = 0
 
-    print(f'Found an {urlparse(result).hostname}, url: '+result)
-    
-    save_result(urlparse(result).hostname,result)
+for result in search:#idk if that work, i have ip block <3
+
+    if nsfw and check_nsfw(result,nsfw,nsfw_count):
+        print('Nsfw Found!')
+
+    elif not check_nsfw(result,nsfw,nsfw_count):
+        print_result(result)
+
+        count+=1
+        if not nsfw:
+            save_result(urlparse(result).hostname,result)
+
+print('Founded '+str(count)+' sites of which '+str(nsfw_count)+' nsfw')
